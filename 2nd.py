ef2ea48d816381633f2a113b9e30b07d854690e9
Roummeljan <40712373+Roummeljan@users.noreply.github.com>
2018-06-30 17:46:33 +0800

    Update 2nd.py

--- 2nd.py
+++ 2nd.py
@@ -1,5 +1,4 @@
 firstname="Fred"
 lastname="Jowson"
 
-#print("First Letter: {} Last Name: {} ".format(firstname[0],lastname[0]))
-print(firstname=[0],lastname=[0])
+print("First Letter: {} Last Name: {} ".format(firstname[0],lastname[0]))
