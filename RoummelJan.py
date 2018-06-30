bb1dcf70a7a9a7a9db98072fa624e0afeda1ce3c
Roummeljan <40712373+Roummeljan@users.noreply.github.com>
2018-06-30 16:31:16 +0800

    starting a new

---
+++ RoummelJan.py
@@ -0,0 +1,21 @@
+# Basic example on how to print
+# text using different approach in
+# Python.
+
+gwapo = 'RoummelJan'
+
+# You can print it right away!
+print('Hi RoummelJan')
+
+# Example of string concatenation
+print('Hello ' + gwapo)
+
+# Using .format() function to attach a string
+print('Hello {}'.format(RoummelJan))
+# print(f'Hello {organization}')  # Can use this format with Python3.6
+
+# First, replace the following text with your name
+your_name = 'Roummel Jan'
+
+# Then, uncomment the next line by removing the "#" sign
+print('{} loves Python!'.format(your_name))
